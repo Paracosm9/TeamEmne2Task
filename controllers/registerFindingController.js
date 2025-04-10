4412018a3605d9aa9a327514396553999014c6db
Paracosm9 <lemniskata9@gmail.com>
2025-04-10 16:33:25 +0300

    Functional view and controller for register finding page

--- controllers/registerFindingController.js
+++ controllers/registerFindingController.js
@@ -2,17 +2,114 @@ function isFieldEmpty(elementId) {
     console.log(elementId);
     let element = document.getElementById(elementId);
     let inputs = model.inputs.registerFinding;
-    if ((elementId == 'linkToWebSite' && inputs.img == '')
+    if ((elementId == 'linkToWebSite' && inputs.img == ''|| elementId == 'linkToWebSite' && !isValidUrl(model.inputs.registerFinding.img))
         || (elementId == 'findPlace' && inputs.locationName == '')
         || (elementId == 'findName' && inputs.name == '')
-        || (elementId == 'findDescription' && inputs.description == ''))
+        || (elementId == 'findDescription' && inputs.description == '' 
+        ))
 
     {
         element.style.border = "2px solid red";
+        return false; 
     }
     else {
         element.style.border = "";
+        return true; 
     }
 
-    // updateView();
+}
+function areAllFieldsNotEmpty(){
+    let fields = ['linkToWebSite', 'findPlace','findName','findDescription']; 
+    let allElementsAreFilled = []; 
+    let allFieldsAreOK = true; 
+    for (const field of fields) {
+        allElementsAreFilled.push(isFieldEmpty(field)); 
+    }
+    for (const fieldStatus of allElementsAreFilled) {
+        if (!fieldStatus){
+            return false; 
+        }
+    }
+    return allFieldsAreOK; 
+}
+
+function isValidUrl(urlToCheck){
+        try { 
+           console.log("Link is valid.") 
+            return Boolean(new URL(urlToCheck)); 
+        }
+        catch(error){ 
+            console.log("Link is not valid.")
+            return false; 
+        }
+}
+function updatePageWithImg(){
+    model.inputs.registerFinding.img = model.inputs.registerFinding.img.replace(".jpeg", ".jpg")
+    updateView();
+    isFieldEmpty('linkToWebSite');
+}
+
+/** More controlls */
+function updateModel(){
+   if ( areAllFieldsNotEmpty() && isValidUrl(model.inputs.registerFinding.img)){
+        let locationId = setNewLocationAndGetID();
+        let idOfNewFinding = setFinding(locationId);
+        setNewImageLink(idOfNewFinding); 
+        
+        updateView();
+        model.inputs.registerFinding.name = '';
+        model.inputs.registerFinding.img = '';
+        model.inputs.registerFinding.locationName = '';
+        model.inputs.registerFinding.description = '';
+   }
+}
+
+function getNewId(dataWithId){
+    let newId = 0; 
+    for (const item of dataWithId) {
+        if (item.id > newId){
+            newId = item.id;
+        }
+    }
+    return newId+1; 
+}
+
+function setNewLocationAndGetID(){
+    let newLocationIndex = getNewId(model.data.location); 
+    let newLocation = {
+        id: newLocationIndex, 
+        name: model.inputs.registerFinding.locationName, 
+        description: "Admin should update the description"
+    }; 
+
+    model.data.location.push(newLocation); 
+    return newLocationIndex; 
+}
+
+function setFinding(locationIdTo){
+    let newFindingIndex = getNewId(model.data.findings); 
+    let newFinding = {
+        id: newFindingIndex, 
+        userId: 2, //change after
+        itemId: newFindingIndex, //also plug, change after
+        locationId: locationIdTo, 
+        dateFind: new Date().toISOString(), 
+        description: model.inputs.registerFinding.description, 
+        acceptedToBePublished: false
+    };
+    model.data.findings.push(newFinding);
+    return newFindingIndex; 
+}
+function setNewImageLink(idOfNewFinding){
+    let newImageIndex = getNewId(model.data.images); 
+    let newImage = {
+        id: newImageIndex, 
+        filePath: model.inputs.registerFinding.img
+    }
+    model.data.images.push(newImage); 
+    model.data.findingImages.push({
+        findingId: idOfNewFinding, 
+        imgId: newImageIndex
+    }); 
+
 }
--- views/registerFindingView.js
+++ views/registerFindingView.js
@@ -1,6 +1,11 @@
-function registerFindingView (){
-
-    return drawView(); 
+function registerFindingView(){
+    if ( model.inputs.registerFinding.name == ''
+        || model.inputs.registerFinding.img == ''
+        || model.inputs.registerFinding.locationName == ''
+        || model.inputs.registerFinding.description == ''){
+    return drawView();
+    } 
+    else return drawThankYouPage();
 }
 
 function drawView(){
@@ -8,21 +13,25 @@ function drawView(){
         <div class = "findingAdd" style = "border: 5px solid black; margin: 20px; display:flex; justify-content: center; flex-direction: column; padding: 5px; ">
         
         <input type="text" id="linkToWebSite" 
-        value = "Legg til linken til image"
-        name="findingLink"
-        onchange = "model.inputs.registerFinding.img = this.value, isFieldEmpty(this.id)"
+        value = "${model.inputs.registerFinding.img == '' ? 'Legg til linken til image' : model.inputs.registerFinding.img}" 
+        name = "findingLink"
+        onchange = "model.inputs.registerFinding.img = this.value, isFieldEmpty(this.id), updatePageWithImg()"
         >
-        <button class = "addButton" id = "regFindButton1">Legg til bildet…</button>
+        <img src="${model.inputs.registerFinding.img == '' || !isValidUrl(model.inputs.registerFinding.img) ? '' : model.inputs.registerFinding.img}" 
+        width = "${model.inputs.registerFinding.img == '' || !isValidUrl(model.inputs.registerFinding.img) ? '' : "100"}"
+        height = "${model.inputs.registerFinding.img == '' || !isValidUrl(model.inputs.registerFinding.img) ? '' : "100"}"
+        >
+
         <br>
         <input type="text" id="findName" 
-        value = "Legg til navn"
-        name="findingName"
+        value = "${model.inputs.registerFinding.name == '' ? 'Legg til navn' : model.inputs.registerFinding.name}"
+        name = "findingName"
         onchange = "model.inputs.registerFinding.name = this.value, isFieldEmpty(this.id)"
         >
 
         <br>
         <textarea id="findDescription" 
-        value = ""
+        value = "${model.inputs.registerFinding.description == '' ? 'Legge til beskrivelse av sted' : model.inputs.registerFinding.description}"
         name="findDescription"
  
         onchange = "model.inputs.registerFinding.description = this.value, isFieldEmpty(this.id)"
@@ -30,16 +39,26 @@ function drawView(){
         </textarea>
         <br><br>
         <input type="text" id="findPlace" 
-        value = "Angi koordinater eller område"
+        value = "${model.inputs.registerFinding.locationName == '' ? 'Angi koordinater eller område' : model.inputs.registerFinding.locationName}"
         name="findPlace"
         onchange = "model.inputs.registerFinding.locationName = this.value, isFieldEmpty(this.id)"
         >
         </div>
 
-        <div class = "findingAdd" style = "border: 5px solid black; margin: 20px; display:flex; justify-content: center; flex-direction: row; padding: 5px; ">
+        <div class = "findingAdd" style = "border: 5px solid black; margin: 20px; display:flex; justify-content: center; flex-direction: row; padding: 5px;">
         <button class = "addButton" id = "regFindButtonOk" onclick = "updateModel()" >Registrer</button>
-        <button class = "addButton" id = "regFindButtonCancel">Avbryt</button>
+        <button class = "addButton" id = "regFindButtonCancel" onclick = "goBackToPage()">Avbryt</button>
         </div>
-    
     `;
+}
+
+function drawThankYouPage(){
+    return /*HTML*/`
+    <h1>Takk for din funn!<h1>
+    <button onclick = "changePageToCatalog()">Gå tilbake til catalog</button>
+    `;
+}
+
+function changePageToCatalog(){ 
+    console.log ("Here would be redirect to catalog!")
 }
